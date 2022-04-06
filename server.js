f14c0075da967608f7fe4f29ae95606963225310
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-05 22:30:54 -0700

    Add listen for app

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const drinks = require('./models/drinks.js')
+
+app.listen(PORT, () => {
+	console.log(`Port ${PORT} has started`)
+})
