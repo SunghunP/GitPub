6a05f0647034cd5cb3079ef56187523c83443345
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-05 22:48:29 -0700

    Create Drinks express application

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const drinks = require('./models/drinks.js')
 
+app.get('/drinks/', (req, res) => {
+	res.render('drinks_index.ejs', {allDrinks : drinks})
+})
+
 app.listen(PORT, () => {
 	console.log(`Port ${PORT} has started`)
 })
