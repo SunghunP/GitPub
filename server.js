c325335fe045333c7e35950bb200a3e6047fb3c8
Sunghun Park <sunghun.josephp@gmail.com>
2022-04-08 15:06:27 -0700

    Create route for show page

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
+///////////////////////////
+// DEPENDENCIES
+///////////////////////////
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+///////////////////////////
+// DATABASE
+///////////////////////////
 const drinks = require('./models/drinks.js')
 
+///////////////////////////
+// ROUTES
+///////////////////////////
 
+// root route redirects to '/drinks/'
 app.get('/', (req, res) => {
 	res.redirect('/drinks/')
 })
 
 app.get('/drinks/', (req, res) => {
-	res.render('drinks_index.ejs', {allDrinks : drinks})
+	res.render('drinks_index.ejs', {drinks : drinks})
+})
+
+app.get('/drinks/:id', (req, res) => {
+	const id = req.params.id
+	res.render('drinks_show.ejs', {drinksId: drinks[id]})
 })
 
 app.listen(PORT, () => {
